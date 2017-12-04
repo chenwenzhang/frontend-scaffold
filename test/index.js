fa6b35c9a02e375dbb6dbf1ddb802d8b231bf9f7
smile <381266902@qq.com>
2017-12-04 13:31:43 +0800

    update version 0.0.5 fix "webpack: module not found" exception

--- test/index.js
+++ test/index.js
@@ -5,8 +5,10 @@ const scaffold = require("../lib/frontend-scaffold");
 scaffold.config({});
 scaffold.setRoot(path.dirname(__dirname) + "/demo");
 
+console.log(__dirname);
+
 // page configs
-const pageConfigs = require("../lib/page-configs");
+// const pageConfigs = require("../lib/page-configs");
 
 // mock servers
 // const mockServers = require("../lib/mock-servers");
