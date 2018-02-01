41e46e50ae07be7693b2bb6219afbbd8e2ccde3d
smile <381266902@qq.com>
2018-02-01 15:12:36 +0800

    modify shell add pageConfig to defaultConfig

--- bin/frontend-scaffold.js
+++ bin/frontend-scaffold.js
@@ -6,16 +6,18 @@ const fs = require("fs");
 const root = process.cwd();
 const filename = "frontend-scaffold.json";
 const file = `${root}/${filename}`;
-
-if (!fs.existsSync(file)) {
-    console.error(`file '${filename}' does not exists`);
-    process.exit(1);
-}
-
-const config = require(file);
 const scaffold = require("../lib/frontend-scaffold");
-scaffold.config(config);
-scaffold.setRoot(root);
+const check = () => {
+    if (!fs.existsSync(file)) {
+        console.error(`file '${filename}' does not exists`);
+        process.exit(1);
+    }
+};
+const initScaffold = () => {
+    let config = require(file);
+    scaffold.config(config);
+    scaffold.setRoot(root);
+};
 
 const yargs = require("yargs");
 const argv = yargs
@@ -25,6 +27,8 @@ const argv = yargs
         builder: {},
         handler: () => {
             try {
+                check();
+                initScaffold();
                 scaffold.init();
                 console.info("project init success");
                 process.exit(0);
@@ -40,6 +44,8 @@ const argv = yargs
         builder: {},
         handler: () => {
             try {
+                check();
+                initScaffold();
                 scaffold.build(() => {
                     process.exit(0);
                 });
@@ -55,6 +61,8 @@ const argv = yargs
         builder: {},
         handler: () => {
             try {
+                check();
+                initScaffold();
                 scaffold.start(() => {
                     process.exit(0);
                 });
@@ -67,3 +75,4 @@ const argv = yargs
     .help("h")
     .alias("h", "help")
     .argv;
+
